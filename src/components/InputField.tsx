05db11c7773e41e49391ba604583ab2e53b01088
Sayand2002 <sayandak2002@gmail.com>
2024-09-24 01:45:18 +0530

    Completed login from validation. Attempted to give book flip animation but failed. changed login women image with brand title

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,6 +1,6 @@
 import { InputFieldProps } from "../types/inputField";
 
-const InputField = ({ label, type, id, placeholder, required = false, value, onChange }: InputFieldProps) => {
+const InputField = ({ label, type, id, placeholder, value, onChange, name }: InputFieldProps) => {
     return (
         <div>
             <label htmlFor={id} className="block text-sm font-medium text-gray-700">
@@ -13,7 +13,7 @@ const InputField = ({ label, type, id, placeholder, required = false, value, onC
                 onChange={onChange}
                 className="w-full px-3 py-2 md:py-3 border border-gray-200 shadow-xs focus:outline-none focus:ring-secondary focus:border-secondary text-sm rounded-lg"
                 placeholder={placeholder}
-                required={required}
+                name={name}
             />
         </div>
     );
